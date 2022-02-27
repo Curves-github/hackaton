a46cfcd8ce3de8bb44cbe81a9ca96ac8c50bb7a6
ArtemFokin <artfokin69@gmail.com>
2022-02-27 23:00:12 +0300

    Add basic near connection with firebase schedule-function

--- firebase-functions/functions/src/index.ts
+++ firebase-functions/functions/src/index.ts
@@ -1,8 +1,44 @@
 import * as functions from 'firebase-functions';
+import { connect, keyStores, utils} from 'near-api-js'
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions
+  .runWith({ secrets: ["NEAR_UPLOADER_CREDENTIALS"] })
+  .https.onRequest((_, response) => {
+    // ${process.env.NEAR_UPLOADER_CREDENTIALS}
+    response.send(`Hello from Firebase!`);
+  });
+
+export const scheduledInsertUnits = functions
+  .runWith({ secrets: ["NEAR_UPLOADER_CREDENTIALS"] })
+  // .pubsub.schedule('0 4 * * *')
+  .pubsub.schedule('every 5 minutes')
+  .timeZone('Europe/Moscow') 
+  .onRun((context) => {
+    const privateKey = process.env.NEAR_UPLOADER_CREDENTIALS;
+    const accountId = process.env.NEAR_UPLOADER_ACCOUNT;
+    if(!privateKey || !accountId){
+      throw new Error(`private key(${privateKey}) or account id(${accountId}) not found`)
+    }
+    const keyPair = utils.KeyPair.fromString(privateKey);
+    const keyStore = new keyStores.InMemoryKeyStore();
+    keyStore.setKey('testnet', accountId, keyPair);
+    const config = { 
+      keyStore, 
+      networkId: 'testnet',
+      nodeUrl: 'https://rpc.testnet.near.org',
+      walletUrl: 'https://wallet.testnet.near.org',
+      helperUrl: 'https://helper.testnet.near.org',
+      explorerUrl: 'https://explorer.testnet.near.org',
+      headers: {}
+    };
+    (async function () {
+      const near = await connect(config);
+      const account = await near.account(accountId);
+      const state = await account.state()
+      console.log({state})
+    })();
+  
+  return null;
+});
--- nft-saver/index.js
+++ nft-saver/index.js
@@ -1,37 +1,33 @@
 const axios = require("axios").default;
 const fs = require("fs")
+const uniqBy = require('lodash.uniqby');
 
-const init = async () => {
+const LIMIT_PER_REQUEST = 50;
 
-  const resp = await axios.get("https://testnets-api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=50")
-  const resp2 = await axios.get("https://testnets-api.opensea.io/api/v1/assets?order_direction=desc&offset=50&limit=50")
-  const resp3 = await axios.get("https://testnets-api.opensea.io/api/v1/assets?order_direction=desc&offset=100&limit=50")
+const init = async (assetsLimit) => {
+  const requetsCount = Math.ceil(assetsLimit / LIMIT_PER_REQUEST); 
+  const requests = new Array(requetsCount).fill(0).map((_,i)=>{
+    const assetsCount = requetsCount - 1 === i ? assetsLimit - i * LIMIT_PER_REQUEST : LIMIT_PER_REQUEST;
+    return axios.get(`https://testnets-api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=${assetsCount}`)
+  })
+  const response = await Promise.all(requests);
+  const assets = uniqBy(response.map(r=>r.data.assets).flat(), 'image_url').map(as=>({url: as.image_url, owner: as.owner.address}));
+  
 
-  const assets = [ ...resp.data.assets, resp2.data.assets, resp3.data.assets ]
-
-  const images = new Set()
-  const arr = []
-  for (let asset of assets) {
-    if (!asset.image_url) continue
-    if (images.has(asset.image_url)) continue
-    images.add(asset.image_url)
-    arr.push(asset)
-  }
-
-  await fs.promises.writeFile("./nft.json", JSON.stringify(arr))
-  console.log(`nft.json writed! Images count: ${arr.length}`)
+  await fs.promises.writeFile("./nft.json", JSON.stringify(assets))
+  console.log(`nft.json writed! Images count: ${assets.length}`)
 
   // const file = await fs.promises.readFile("./nft.json")
   // const arr = JSON.parse(file)
 
-  for (let file of arr) {
-    const resp = await axios.get(file.image_url, {
-      responseType: "arraybuffer"
-    })
-    await fs.promises.writeFile(`./images/${file.id}.png`, resp.data)
-    console.log(`Image ${file.id}.png downloaded. (${resp.data.length} bytes)`)
-  }
+  // for (let file of arr) {
+  //   const resp = await axios.get(file.image_url, {
+  //     responseType: "arraybuffer"
+  //   })
+  //   await fs.promises.writeFile(`./images/${file.id}.png`, resp.data)
+  //   console.log(`Image ${file.id}.png downloaded. (${resp.data.length} bytes)`)
+  // }
 
 }
 
-init()
+init(50)
